afa88ddd3c5afe285d5dd58ae1e6fc4a4d11b2ff
ABHISHEKyadav281 <yyadavabhishek9@gmail.com>
2023-12-21 12:23:57 +0530

    vercel fixed

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use("/api/auth", authRoutes);
 
 
 
-app.listen(`dummy-three-vert.vercel.app/`,(req,res,next) => {
+app.listen(8080,(req,res,next) => {
     try {connect()
     console.log("connected to server")}
     catch (err) {
